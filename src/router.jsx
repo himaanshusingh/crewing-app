4127d4ba309075c890da9eebb72b9051eefc4dfd
Himanshu Singh <himanshusingh1k@gmail.com>
2026-03-15 20:34:43 +0530

    fix base path

--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
-const router = createBrowserRouter([
+const router = HashRouter([
   {
     path: "/",
     element: <App />,
